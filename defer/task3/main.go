ebc28ef7d8e1f7140ad5638f96f5f07172eb9121
Egor <555666741@mail.ru>
2026-09-17 17:15:53 +0500

    defer/task3/case1 вывод - 	without: <nil> Default error 	with: <nil> Default error
    defer выполняется после окончания функции; функция уже вернула значение, а затем выполнился defer

---
+++ defer/task3/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	println("Case 1")
+	case1()
+	println()
+	println()
+
+	println("Case 2")
+	case2()
+	println()
+	println()
+	/*
+		println("Case 3")
+		case3()
+		println()
+		println()
+	*/
+}
+
+func case1() {
+	helperWithDefer := func(isError bool) error {
+		var retVal error
+
+		defer func() {
+			retVal = errors.New("Extra error")
+		}()
+
+		if isError {
+			retVal = errors.New("Default error")
+		}
+
+		return retVal
+	}
+
+	helperWithoutDefer := func(isError bool) error {
+		var retVal error
+
+		if isError {
+			retVal = errors.New("Default error")
+		}
+
+		return retVal
+	}
+
+	fmt.Println("\twithout:")
+	fmt.Println(helperWithoutDefer(false))
+	fmt.Println(helperWithoutDefer(true))
+	fmt.Println("\twith:")
+	fmt.Println(helperWithDefer(false))
+	fmt.Println(helperWithDefer(true))
+}
+
+/*
+вывод -
+	without:
+<nil>
+Default error
+	with:
+<nil>
+Default error
+
+defer выполняется после окончания функции; функция уже вернула значение, а затем выполнился defer
+
+*/
+
+func case2() {
+	helperWithDefer := func(isError bool) (retVal error) {
+		defer func() {
+			retVal = errors.New("Extra error")
+		}()
+
+		if isError {
+			retVal = errors.New("Default error")
+		}
+
+		return
+	}
+
+	helperWithoutDefer := func(isError bool) (retVal error) {
+		if isError {
+			retVal = errors.New("Default error")
+		}
+
+		return
+	}
+
+	fmt.Println("\twithout:")
+	fmt.Println(helperWithoutDefer(false))
+	fmt.Println(helperWithoutDefer(true))
+	fmt.Println("\twith:")
+	fmt.Println(helperWithDefer(false))
+	fmt.Println(helperWithDefer(true))
+}
+
+func case3() {
+	helperWithDefer := func(isError bool) (retVal error) {
+		defer func() {
+			retVal = errors.New("First Error")
+		}()
+
+		defer func() {
+			retVal = errors.New("Second Error")
+		}()
+
+		if isError {
+			retVal = errors.New("Default error")
+		}
+
+		return
+	}
+
+	helperWithoutDefer := func(isError bool) (retVal error) {
+		if isError {
+			retVal = errors.New("Default error")
+		}
+
+		return
+	}
+
+	fmt.Println("\twithout:")
+	fmt.Println(helperWithoutDefer(false))
+	fmt.Println(helperWithoutDefer(true))
+	fmt.Println("\twith:")
+	fmt.Println(helperWithDefer(false))
+	fmt.Println(helperWithDefer(true))
+}
