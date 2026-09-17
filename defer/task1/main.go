99013ccc2da4cafa69955841b47e39f75a242ba5
Egor <555666741@mail.ru>
2026-09-17 16:19:30 +0500

    defer/task1 вывод - end 4 3 2 1 defer выполняются после окончания основной функции в LIFO порядке

--- defer/task1/main.go
+++ defer/task1/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("start")
+	for i := 1; i < 4; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("end")
+}
+
+/*
+end
+4
+3
+2
+1
+*/
