80bda79e00dee0c9cb0fdb20594e57ee75558b67
Egor <555666741@mail.ru>
2026-09-19 20:15:19 +0500

    panics/task1

--- panics/task1/main.go
+++ panics/task1/main.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+func CausePanic() {
+	panic("что-то пошло не так!")
+}
+
+func HandlePanic() {
+	defer func() {
+		if pan := recover(); pan != nil {
+			fmt.Printf("Паника перехвачена: %v \n", pan)
+		}
+	}()
+
+	CausePanic()
+}
+func main() {
+	//CausePanic()
+	HandlePanic()
+}
